7cfc84a2ea3480ad81c53c04d3ad3ba5766edcba
barkode <konstantinks@gmail.com>
2024-06-30 21:27:29 +0100

    Add code to calculate two random numbers for the game question

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,12 +16,29 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 });
 
-function runGame() {}
+/**
+ * The main game "loop", called when the script is first time loaded
+ * and after the user's answer hs been processed
+ */
+
+function runGame() {
+  // Create two random numbers between 1 and 25
+  let num1 = Math.floor(Math.random() * 25) + 1;
+  let num2 = Math.floor(Math.random() * 25) + 1;
+}
+
 function checkAnswer() {}
+
 function calculateCorrectAnswer() {}
+
 function incrementScore() {}
+
 function incrementWrongAnswer() {}
+
 function displayAdditionalQuestion() {}
+
 function displaySubtractQuestion() {}
+
 function displayMultiplyQuestion() {}
+
 function displayDivisionQuestion() {}
